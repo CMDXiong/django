853464559766e50806cb2021e001aeada2b9458d
PanXiong <1357211280@qq.com>
2020-03-09 17:35:13 +0800

    abstracterbaseuser扩展模型

---
+++ anthenticate_abstracterbaseuser/front/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth import get_user_model
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, telephone, username, password, **kwargs):
+        if not telephone:
+            raise ValueError('必须要传递手机号码')
+        if not password:
+            raise ValueError('必须要传递密码')
+        user = self.model(telephone=telephone, username=username, **kwargs)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_user(self, telephone, username, password, **kwargs):
+        kwargs['is_superuser'] = False
+        return self._create_user(telephone=telephone, username=username, password=password, **kwargs)
+
+    def create_superuser(self, telephone, username, password, **kwargs):
+        kwargs['is_superuser'] = True
+        return self._create_user(telephone=telephone, usernane=username, password=password, **kwargs)
+
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    telephone = models.CharField(max_length=11, unique=True)
+    email = models.CharField(max_length=100, unique=True)
+    username = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'telephone'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def get_full_name(self):
+        return self.username
+
+    def get_short_name(self):
+        return self.username
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
---
+++ anthenticate_abstracterbaseuser/front/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import MyUser
+from django.contrib.auth import authenticate
+
+
+def inherit_view(request):
+    telephone = '17788889999'
+    password = '111111'
+    email = '123@qq.com'
+    username = 'px'
+    user = MyUser.objects.create_user(telephone=telephone, username=username,
+                                      password=password, email=email)
+    print(user.username)
+    return HttpResponse('继承AbstracterUser扩展用户')
+
+
+def authen(request):
+    telephone = '17788889999'
+    password = '111111'
+    user = authenticate(request, username=telephone,password=password)
+    if user:
+        print(user.username)
+    return HttpResponse('继承AbstracterUser扩展用户的验证')
